b9c1843dd124a934f58ea356998ce1f695dd6049
safakferhatkaya <safakferhatkaya@gmail.com>
2024-02-06 18:55:44 +0300

    luxon importe

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin 'sortablejs', to: 'https://ga.jspm.io/npm:sortablejs@1.15.0/modular/sortable.esm.js'
+pin 'luxon', to: 'https://ga.jspm.io/npm:luxon@3.4.3/src/luxon.js'
